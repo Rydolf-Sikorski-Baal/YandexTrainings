b54e1d22e0e069c2c62aa3d043a1b7c5a616f3d2
Rydolf-Sikorski-Baal <72573127+Rydolf-Sikorski-Baal@users.noreply.github.com>
2023-04-10 16:55:14 +0300

    Create 23.cpp

---
+++ 23.cpp
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma GCC optimize("03")
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <string>
+#include <stack>
+#include <map>
+#include <utility>
+#include <queue>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep_1(i, n) for (int i = 1; i < n; ++i)
+#define rep_s(i, s, n) for (int i = s; i < n; ++i)
+#define load_ll(a) ll a; cin >> a;
+#define load_2ll(a, b) ll a, b; cin >> a >> b;
+#define load_3ll(a, b, c) ll a, b, c; cin >> a >> b >> c;
+#define load_str(a) string a; cin >> a;
+#define max_it(a, b) a = max(a, b);
+#define min_it(a, b) a = min(a, b);
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef stack<ll> sl;
+typedef stack<pll> spll;
+
+void fast(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+void file(){
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+}
+void start(){
+  fast(); 
+  file();
+}
+
+ll bin_p(vl & a, ll p){
+    ll left = 0, right = a.size();
+    while(left + 1 != right){
+        ll mid = (left + right) >> 1;
+        //cout << left << " " << right << " " << mid << '\n';
+        if (mid >= a.size()) return -1;
+        if (a[mid] < p){
+            left = mid;
+        }else{
+            right = mid;
+        }
+    }
+    return left;
+}
+
+inline void solve(){
+    load_ll(n);
+    ll ans = 0;
+    rep_1(i, n + 1) ans += i * (n - i + 1);
+    for (size_t i = 2; i <= n; i += 2) ans += (2*(n - i + 1) - 1) * (i / 2);
+    cout << ans;
+}
+
+int main() {
+	start();
+    ll q = 1;
+    while(q--) solve();
+}
