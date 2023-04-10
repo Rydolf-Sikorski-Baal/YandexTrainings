42972e9b8cd9ed48d0edce01b950c4b82a26b5d8
Rydolf-Sikorski-Baal <72573127+Rydolf-Sikorski-Baal@users.noreply.github.com>
2023-04-10 16:58:51 +0300

    Create 34.cpp

---
+++ 34.cpp
@@ -0,0 +1,177 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma GCC optimize("03")
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <string>
+#include <stack>
+#include <map>
+#include <utility>
+#include <queue>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep_1(i, n) for (int i = 1; i < n; ++i)
+#define rep_s(i, s, n) for (int i = s; i < n; ++i)
+#define load_ll(a) ll a; cin >> a;
+#define load_2ll(a, b) ll a, b; cin >> a >> b;
+#define load_3ll(a, b, c) ll a, b, c; cin >> a >> b >> c;
+#define load_str(a) string a; cin >> a;
+#define max_it(a, b) a = max(a, b);
+#define min_it(a, b) a = min(a, b);
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef stack<ll> sl;
+typedef stack<pll> spll;
+typedef priority_queue<pii> pq_pii;
+typedef map<int, int> mii;
+
+void fast(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+void file(){
+	freopen("joseph.in", "r", stdin);
+	freopen("joseph.out", "w", stdout);
+}
+void start(){
+  fast(); 
+  //file();
+}
+
+vvi graph;
+vi is;
+vi ans;
+
+void dfs(int v, int color){
+    is[v] = 1;ans[v] = color;
+    for (int to : graph[v])
+        if (!is[to]) dfs(to, 1 - color);
+}
+
+bool check(vl & x_c, vl & y_c, ld r){
+    size_t n = x_c.size();
+    graph.clear();graph.resize(n);
+    is.assign(n, 0);
+    ans.assign(n, -1);
+    size_t k = 0;
+    rep(i, n)
+        rep(j, n){
+            if (i == j) continue;
+            ld x = x_c[i] - x_c[j]; x *= x;
+            ld y = y_c[i] - y_c[j]; y *= y;
+            
+            if (x + y <= (ld) 4 * r * r){
+                k++;
+                graph[i].push_back(j);
+                graph[j].push_back(i);
+            }
+        }
+    rep (i, n) if (!is[i]) dfs(i, 1);
+    rep(i, n){
+        for (int to : graph[i]) if (ans[to] != 1 - ans[i]) return false;
+    }
+    return true;
+}
+
+const ld eps = 0.000000000001;
+ld bin_p(vl & x_c, vl & y_c){
+    ld left = 0, right = 1e5;
+    while(right - left > eps){
+        ld mid = (left + right) / 2.0;
+        //cout << left << " " << right << " " << mid << '\n';
+        if (check(x_c, y_c, mid)){
+            left = mid;
+        }else{
+            right = mid;
+        }
+    }
+    return left;
+}
+
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+
+vvi comp;
+vvi zone;
+void dfs_comp(int x, int y, int n){
+    comp[x][y] = n;
+    int k = zone[x][y];
+    
+    if (x) 
+        if ((comp[x - 1][y] == -1) && (zone[x - 1][y] == k)) 
+            dfs_comp(x - 1, y, n);
+    if (y) 
+        if ((comp[x][y - 1] == -1) && (zone[x][y - 1] == k)) 
+            dfs_comp(x, y - 1, n);
+    if (x < comp.size() - 1) 
+        if ((comp[x + 1][y] == -1) && (zone[x + 1][y] == k)) 
+            dfs_comp(x + 1, y, n);
+    if (y < comp[0].size() - 1) 
+        if ((comp[x][y + 1] == -1) && (zone[x][y + 1] == k)) 
+            dfs_comp(x, y + 1, n);
+}
+
+inline void solve(){
+    int n, m; cin >> n >> m;
+    
+    comp.assign(n, vi(m, -1));
+    zone.assign(n, vi(m, 0));
+    rep(i, n) rep(j, m) cin >> zone[i][j];
+    
+    int k = 0;
+    rep(i, n) rep(j, m) if (comp[i][j] == -1) dfs_comp(i, j, k++);
+    //rep(i, n){rep(j, m) cout << comp[i][j] << " "; cout << '\n';}
+    
+    
+    graph.resize(k);
+    rep(i, n) rep_1(j, m){
+        if (zone[i][j - 1] < zone[i][j]) graph[comp[i][j]].push_back(comp[i][j - 1]);
+        if (zone[i][j - 1] > zone[i][j]) graph[comp[i][j - 1]].push_back(comp[i][j]);
+    } 
+    rep_1(i, n) rep(j, m){
+        if (zone[i - 1][j] < zone[i][j]) graph[comp[i][j]].push_back(comp[i - 1][j]);
+        if (zone[i - 1][j] > zone[i][j]) graph[comp[i - 1][j]].push_back(comp[i][j]);
+    } 
+    
+    size_t res = 0;
+    //rep(i, k) cout << gr 
+    rep(i, k) if (!graph[i].size()) res++;
+    cout << res;
+}
+
+int main() {
+    start();
+    int q = 1;
+    while(q--) solve();
+}
